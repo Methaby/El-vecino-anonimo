8be367642335a2e47e85b94333ac0d7763468c05
Methaby <holmerflashxd@gmail.com>
2026-05-08 18:27:00 -0400

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,36 @@
+const CACHE_NAME = 'vecino-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com'
+];
+
+// 1. Instalación: Guarda los archivos esenciales
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 2. Activación: Limpia versiones viejas
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// 3. Fetch: Permite que funcione offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return new Response("Offline mode active");
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
