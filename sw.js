5f736c3fc0aab4b7b47e166909ce8a532c4c0d31
Methaby <holmerflashxd@gmail.com>
2026-05-08 23:38:39 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'vecino-cache-v1';
+const assets = ['./', './index.html'];
+​self.addEventListener('install', e => {
+e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+});
+​self.addEventListener('fetch', e => {
+e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
