b0b8b7afb6508af5578e0c1ec0b9f8b9a9ff6ee3
Methaby <holmerflashxd@gmail.com>
2026-05-10 19:44:53 -0400

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,25 @@
-const CACHE_NAME = 'vecino-cache-v1';
-const assets = ['./', './index.html'];
-​self.addEventListener('install', e => {
-e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+const CACHE_NAME = 'vecino-cache-v2'; // Cambié a v2 para que el navegador fuerce la actualización
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => {
+      console.log('Cacheando archivos de la PWA...');
+      return c.addAll(assets);
+    })
+  );
 });
-​self.addEventListener('fetch', e => {
-e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => {
+      return r || fetch(e.request);
+    })
+  );
 });
